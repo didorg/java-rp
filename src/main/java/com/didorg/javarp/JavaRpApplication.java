16133fddbe4befe2d62ea0b5dc9737b9a7b5dce8
Didier <didorg@gmail.com>
2022-06-23 18:13:15 -0400

    first commit

---
+++ src/main/java/com/didorg/javarp/JavaRpApplication.java
@@ -0,0 +1,13 @@
+package com.didorg.javarp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/didorg/javarp/JavaRpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didorg.javarp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
